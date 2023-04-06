df9ec1e7aa38e74a072aac31f820a956e037e729
sophie <sophieh@lindenschool.ca>
2023-04-06 08:55:02 -0400

    Updated CSS and added web responsive breakpoints

--- script.js
+++ script.js
@@ -24,13 +24,12 @@ function generate_code() {
 
 }
 
-
 const pages = {
 
     active_page: function (page_name, student_code, room_code) {
         this[page_name](student_code, room_code)
         var page = document.getElementById(page_name)
-        page.style.display = "block"
+        page.style.display = "grid"
         this.page_arr.forEach(e => {
             if (e != page_name) {
                 document.getElementById(e).style.display = "none"
@@ -57,7 +56,7 @@ const pages = {
         const page_div = document.getElementById("start_page")
 
         const title = document.createElement("h1")
-        title.innerHTML = "EducateHacks: Note-Taking Tracker"
+        title.innerHTML = "Note-Taking Tracker"
         page_div.appendChild(title)
 
         const enter_btn = document.createElement("button")
@@ -88,27 +87,40 @@ const pages = {
         enter_btn.innerHTML = "Enter"
         enter_btn.classList = "celestial"
 
+        const code_err = document.createElement("p")
+        code_err.innerHTML = "⚠ Code does not correspond to any existing room"
+        code_err.style.cssText = "color: #FE615C; font-size: 12px; visibility: hidden"
+        page_div.appendChild(code_err)
+
         enter_btn.onclick = function () {
             var entry_code = code_input.value
-            database.ref().child(entry_code).get().then((result) => {
-                // If the room doesn't exist, there should be an alert
-                if (result.exists()) {
-                    var student_code = generate_code()
-                    database.ref(`${entry_code}/students/${student_code}`).set({
-                        repeat: false,
-                        slow_down: false,
-                        elaborate: false,
-                        is_typing: false,
-                    })
-                    pages.active_page("student_page", student_code, entry_code)
-                } else {
-                    console.log("Code does not exist")
-                }
-            }).catch((err) => {
-                console.log(err)
-            })
+            if (entry_code !== "") {
+                database.ref().child(entry_code).get().then((result) => {
+                    if (result.exists()) {
+                        var student_code = generate_code()
+                        database.ref(`${entry_code}/students/${student_code}`).set({
+                            repeat: false,
+                            slow_down: false,
+                            elaborate: false,
+                            is_typing: false,
+                        })
+                        pages.active_page("student_page", student_code, entry_code)
+                    } else {
+                        display_code_err()
+                    }
+                }).catch((err) => {
+                    console.log(err)
+                })
+            } else {
+                display_code_err()
+            }
+
         }
 
+        function display_code_err() {
+            console.log("Code does not exist")
+            code_err.style.visibility = "visible"
+        }
     },
 
     student_page: function (student_code, room_code) {
@@ -118,15 +130,18 @@ const pages = {
 
         const textarea = document.createElement("textarea")
         textarea.placeholder = "Type your notes here!"
+        textarea.style.gridArea = "1 / 1 / 1 / span 3"
         page_div.appendChild(textarea)
 
-        const btn_names = ["Repeat ⟳", "Slow down ↓", "Elaborate 🛈"]
+        var btn_names = ["Repeat ⟳", "Slow down ↓", "Elaborate 🛈"]
+
         const btn_ids = ["repeat", "slow_down", "elaborate"]
         const btn_colors = ["orange", "bittersweet", "celestial"]
 
         for (i = 0; i < 3; i++) {
             var btn = document.createElement("button")
             btn.innerHTML = btn_names[i]
+            btn.style.cssText = `grid-area: 2 / ${i + 1} / 2 / ${i + 1}`
             btn.id = btn_ids[i]
             btn.classList = btn_colors[i]
             page_div.appendChild(btn)
@@ -157,7 +172,7 @@ const pages = {
                     btn.disabled = false;
                 }
 
-                setTimeout(set_false, 5000)
+                setTimeout(set_false, 2000)
 
             }
         })
@@ -175,6 +190,22 @@ const pages = {
 
         })
 
+        const room_disbanded = document.createElement("p")
+        room_disbanded.innerHTML = "Your teacher has closed the room, but you may continue taking notes if you wish."
+        room_disbanded.style.cssText = "visibility: hidden; font-size: 12px; grid-column: 1 / span 3"
+        page_div.appendChild(room_disbanded)
+
+        var room_ref = database.ref(room_code)
+        room_ref.on("value", function () {
+            room_ref.get().then((result) => {
+                if (!(result.exists())) {
+                    console.log("hello")
+                    room_disbanded.style.visibility = "visible"
+                }
+            })
+        })
+
+
         // If teacher-created room disappears, there should be some sort of notification
         window.onbeforeunload = (e) => {
             student_ref.remove()
@@ -187,9 +218,15 @@ const pages = {
         this.create_page_div("teacher_page")
         const page_div = document.getElementById("teacher_page")
 
+        const room_code = document.createElement("p")
+        room_code.innerHTML = "Room Code"
+        room_code.style.cssText = "margin: 0; padding: 0;"
+        page_div.appendChild(room_code)
+
         const code = generate_code()
-        const p_code = document.createElement("p")
-        p_code.innerHTML = `Room code: ${code}`
+        const p_code = document.createElement("h1")
+        p_code.style.cssText = "margin: 0"
+        p_code.innerHTML = code
         page_div.appendChild(p_code)
 
         const room_ref = database.ref("/" + code)
@@ -200,11 +237,60 @@ const pages = {
         })
 
         const info_arr = ["students_typing", "students_repeat", "students_slow_down", "students_elaborate"]
+        const icon_arr = ["https://cdn-icons-png.flaticon.com/512/1162/1162933.png",
+            "https://cdn-icons-png.flaticon.com/512/2611/2611386.png",
+            "https://cdn-icons-png.flaticon.com/512/7549/7549353.png",
+            "https://cdn-icons-png.flaticon.com/512/4786/4786206.png"]
+
+        const headers = ["Typing", "Repeat", "Slower", "Elaborate"]
+        const status_icons = ["https://cdn-icons-png.flaticon.com/512/8072/8072916.png", "https://cdn-icons-png.flaticon.com/512/8072/8072917.png"]
+        const rgb_codes = ["rgb(110, 37, 148, 0.1)", "rgb(255, 167, 41, 0.1)", "rgb(254, 97, 92, 0.1)", "rgb(5, 142, 217, 0.1)",]
 
         for (i = 0; i < 4; i++) {
+            var div = document.createElement("div")
+            div.id = `div_${i}`
+            div.style.cssText = "display: grid; grid-template-rows: 2fr 2fr 1fr; grid-template-columns: 1fr 4fr 1fr; margin: 20px; border-radius: 20px; overflow: hidden"
+            div.style.backgroundColor = rgb_codes[i]
+            page_div.appendChild(div)
+
+            var img = document.createElement("img")
+            img.src = icon_arr[i]
+            img.style.cssText = "height: 64px; place-self: center; grid-area: 1 / 1 / span 2 / 1; margin: 20px 0 20px 20px"
+            div.appendChild(img)
+
             var p = document.createElement("p")
+            p.style.cssText = "grid-area: 2 / 2 / 2 / 2; place-self: start; margin: 0 20px"
             p.id = info_arr[i]
-            page_div.appendChild(p)
+            div.appendChild(p)
+
+            var color_div = document.createElement("div")
+            color_div.style.cssText = "grid-area: 3 / 1 / 3 / span 3"
+            color_div.id = `color_div${i}`
+            div.appendChild(color_div)
+
+            var header = document.createElement("h3")
+            header.innerHTML = headers[i]
+            header.style.cssText = "grid-area: 1 / 2 / 1 / 2; place-self: end start; margin: 5px 20px"
+            div.appendChild(header)
+
+            var status_icon = document.createElement("img")
+            status_icon.style.cssText = "height: 48px; align-self: center; grid-area: 1 / 3 / span 2 / 3"
+            status_icon.id = `status_icon${i}`
+            div.appendChild(status_icon)
+        }
+
+        function status_switch(id_num, bCheck) {
+            var status_icon = document.getElementById(`status_icon${id_num}`)
+            var color_div = document.getElementById(`color_div${id_num}`)
+            if (bCheck) {
+                status_icon.src = status_icons[0]
+                color_div.classList = "emerald"
+            } else {
+                status_icon.src = status_icons[1]
+                color_div.classList = "bittersweet"
+                console.log(status_icon.src, color_div.classList)
+            }
+
         }
 
         const no_students = document.createElement("p")
@@ -212,7 +298,6 @@ const pages = {
         page_div.appendChild(no_students)
 
         room_ref.on('value', (snapshot) => {
-
             room_ref.child("students").get().then((result) => {
 
                 if (result.exists()) {
@@ -221,60 +306,54 @@ const pages = {
                     var student_obj = room_obj["students"]
                     var total_students = Object.keys(student_obj).length
                     var students_typing = students_repeat = students_slow_down = students_elaborate = 0
-                    var typing_status = repeat_status = slow_status = elaborate_status = "✓"
 
-                    info_arr.forEach(e => {
-                        document.getElementById(e).classList = "display_info emerald"
-                    })
+                    for (i = 0; i < 4; i++) {
+                        document.getElementById(`div_${i}`).style.display = "grid"
+                        status_switch(i, true)
+                    }
 
                     for (student in student_obj) {
                         if (student_obj[student]["is_typing"]) {
                             students_typing = + 1
-                            typing_status = "✗"
-                            document.getElementById("students_typing").classList.replace("emerald", "rebecca")
+                            status_switch(0, false)
                         }
                     }
 
                     for (student in student_obj) {
                         if (student_obj[student]["repeat"]) {
                             students_repeat = + 1
-                            repeat_status = "✗"
-                            document.getElementById("students_repeat").classList.replace("emerald", "orange")
+                            status_switch(1, false)
                         }
                     }
 
                     for (student in student_obj) {
                         if (student_obj[student]["slow_down"]) {
                             students_slow_down = + 1
-                            slow_status = "✗"
-                            document.getElementById("students_slow_down").classList.replace("emerald", "bittersweet")
+                            status_switch(2, false)
                         }
                     }
 
                     for (student in student_obj) {
                         if (student_obj[student]["elaborate"]) {
                             students_elaborate = + 1
-                            elaborate_status = "✗"
-                            document.getElementById("students_elaborate").classList.replace("emerald", "celestial")
+                            status_switch(3, false)
                         }
                     }
 
-                    info_arr.forEach((e) => {
-                        document.getElementById(e).style.display = "block"
-                    })
                     no_students.style.display = "none"
 
-                    document.getElementById("students_typing").innerHTML = `${typing_status} ${students_typing}/${total_students} are typing`
-                    document.getElementById("students_elaborate").innerHTML = `${elaborate_status} ${students_elaborate}/${total_students} want elaboration`
-                    document.getElementById("students_slow_down").innerHTML = `${slow_status} ${students_slow_down}/${total_students} want a slower pace`
-                    document.getElementById("students_repeat").innerHTML = `${repeat_status} ${students_repeat}/${total_students} want repetition`
+                    document.getElementById("students_typing").innerHTML = `${students_typing}/${total_students} students are typing`
+                    document.getElementById("students_elaborate").innerHTML = `${students_elaborate}/${total_students} students want you to elaborate`
+                    document.getElementById("students_slow_down").innerHTML = `${students_slow_down}/${total_students} want you to slow down`
+                    document.getElementById("students_repeat").innerHTML = `${students_repeat}/${total_students} want you to repeat yourself`
 
                 } else {
 
-                    info_arr.forEach((e) => {
-                        document.getElementById(e).style.display = "none"
-                    })
-                    no_students.style.display = "block"
+                    for (i = 0; i < 4; i++) {
+                        document.getElementById(`div_${i}`).style.display = "none"
+                    }
+
+                    no_students.style.display = "grid"
 
                 }
             })
@@ -289,5 +368,3 @@ const pages = {
 }
 
 pages.active_page("start_page")
-
-
